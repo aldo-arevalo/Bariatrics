ff2bbaa5f4a76fc5855a7f3886c61a3ef6ee58bd
Aldo Arévalo <aldoare@mit.edu>
2020-10-12 14:40:35 +0100

    Adding documentation comments.

--- Modeling/InterpepretabilityPaper/Functions/getPartitions.m
+++ Modeling/InterpepretabilityPaper/Functions/getPartitions.m
@@ -1,5 +1,25 @@
 function [labels,cvp1,data_train,data_test,cvp2] = getPartitions...
     (data_table, filename_partitions, Options)
+% Function that creates the training/validation and testing subsets. Also
+% creates the labels for comprehensive reading of plots.
+%
+% INPUTS:
+%       data_table:          Input dataset (table).
+%       filename_partitions: String containing the path to save files.
+%       Options:             Structure that contains the general settings.
+%
+% OUTPUTS:
+%       labels:     Vector containing the feature names for printing.
+%       cvp1:       Stratified hold out partition.
+%       data_train: Data set for training (x features, y output variable).
+%       data_test:  Data for testing (x features, y output variable).
+%       cvp2:       Stratified cross validation partition.
+%
+% Dependencies: getDataset
+%
+% Author: Aldo Arévalo
+% Date: 10/11/2020
+
 % Generate labels for plots
 labels = getVarsLabels(data_table.Properties.VariableNames,Options);
 
